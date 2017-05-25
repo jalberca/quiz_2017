f413a57ba36342c413caad5f704ac60dc5063184
jalberca <javieralberca27@gmail.com>
2017-05-25 14:54:21 +0200

    Practica 52 Quiz

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ router.get('/author', function(req, res, next) {
 });
 
 
+
 // Autoload de rutas que usen :quizId
 router.param('quizId', quizController.load);
 
@@ -29,6 +30,13 @@ router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
 
 router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
+
+router.get('/quizzes/randomplay',  quizController.randomplay);
+router.get('/quizzes/randomcheck/:quizId(\\d+)', quizController.randomcheck);
+
+router.get('/quizzes/randomresult', function(req, res, next) {
+    res.render('quizzes/random_result');
+});
 // Pagina de ayuda
 router.get('/help', function(req, res, next) {
     res.render('help');
